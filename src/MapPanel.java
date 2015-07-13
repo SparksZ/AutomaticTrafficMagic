44e3759533aa490258e840f1b4ccc1b92ef78956
tdurk93 <tdurk93@gmail.com>
2015-07-12 21:52:25 -0400

    Painting sinks
    Also added antialiasing with bilinear interpolation to make it look
    pretty.

--- src/MapPanel.java
+++ src/MapPanel.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Dimension;
 import java.util.List;
+import java.awt.RenderingHints;
 
 public class MapPanel extends JPanel {
     private int sideLength;
@@ -13,7 +14,7 @@ public class MapPanel extends JPanel {
     private static final int INTERSECTION_OVAL_DIAM = 5;
     private static int numIntersectionsPerSide;
     private static final int PADDING = 20; // not pixels, but proportional to pixels
-    private static final int vizRoadWidth = 10;
+    private static final int vizRoadWidth = 16;
 
     public MapPanel(int sideLength, List<Intersection> intersections) {
         numIntersectionsPerSide = (int)Math.round(Math.sqrt(intersections.size()));
@@ -25,13 +26,16 @@ public class MapPanel extends JPanel {
 
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         scalingFactorX = (double)getWidth()/(2*PADDING + sideLength);
         scalingFactorY = (double)getHeight()/(2*PADDING + sideLength);
         for (int index = 0; index < intersections.size(); index++) {
-            //g2.setColor(Color.GRAY);
             Intersection i = intersections.get(index);
             boolean currLightState = i.getState();
-            // east-west
+            // paint intersections, with corresponding light colors
             if (currLightState) {
                 g2.setColor(Color.RED);
                 g2.fillRect(panelX(i.getX() - 75), panelY(i.getY() - vizRoadWidth),
@@ -49,33 +53,25 @@ public class MapPanel extends JPanel {
                         (int)(150*scalingFactorX), (int)(vizRoadWidth*2*scalingFactorY));
             }
 
-            // north-south
-            if (currLightState) {
-                g2.setColor(Color.GREEN);
-            }
-            else {
-                g2.setColor(Color.RED);
-            }
-            g2.fillRect(panelX(i.getX() - vizRoadWidth), panelY(i.getY() - 75),
-                    (int)(vizRoadWidth*2*scalingFactorX), (int)(150*scalingFactorY));
             List<Road> interRoads = i.getRoads();
             g2.setColor(Color.GRAY);
             Road currRoad;
+            // southbound roads are on the left, eastbound roads are on top
             // north-south inbound top
             currRoad = interRoads.get(0);
-            g2.fillRect(panelX(currRoad.getXWestPos() + vizRoadWidth/2), panelY(currRoad.getYNorthPos()),
+            g2.fillRect(panelX(currRoad.getXWestPos() - vizRoadWidth), panelY(currRoad.getYNorthPos()),
                     (int)(vizRoadWidth/2*scalingFactorX), (int)((currRoad.getYSouthPos() - currRoad.getYNorthPos())*scalingFactorY));
             // north-south outbound bottom
             currRoad = interRoads.get(6);
-            g2.fillRect(panelX(currRoad.getXWestPos() + vizRoadWidth/2), panelY(currRoad.getYNorthPos()),
+            g2.fillRect(panelX(currRoad.getXWestPos() - vizRoadWidth), panelY(currRoad.getYNorthPos()),
                     (int)(vizRoadWidth/2*scalingFactorX), (int)((currRoad.getYSouthPos() - currRoad.getYNorthPos())*scalingFactorY));
             // north-south outbound top
             currRoad = interRoads.get(4);
-            g2.fillRect(panelX(currRoad.getXWestPos() - vizRoadWidth), panelY(currRoad.getYNorthPos()),
+            g2.fillRect(panelX(currRoad.getXWestPos() + vizRoadWidth/2), panelY(currRoad.getYNorthPos()),
                     (int)(vizRoadWidth/2*scalingFactorX), (int)((currRoad.getYSouthPos() - currRoad.getYNorthPos())*scalingFactorY));
             // north-south inbound bottom
             currRoad = interRoads.get(2);
-            g2.fillRect(panelX(currRoad.getXWestPos() - vizRoadWidth), panelY(currRoad.getYNorthPos()),
+            g2.fillRect(panelX(currRoad.getXWestPos() + vizRoadWidth/2), panelY(currRoad.getYNorthPos()),
                     (int)(vizRoadWidth/2*scalingFactorX), (int)((currRoad.getYSouthPos() - currRoad.getYNorthPos())*scalingFactorY));
             // east-west inbound left
             currRoad = interRoads.get(3);
@@ -94,12 +90,38 @@ public class MapPanel extends JPanel {
             g2.fillRect(panelX(currRoad.getXWestPos()), panelY(currRoad.getYNorthPos() + vizRoadWidth/2),
                         (int)((currRoad.getXEastPos() - currRoad.getXWestPos())*scalingFactorX), (int)(vizRoadWidth/2*scalingFactorY));
 
-
+            // paint sinks
+            g2.setColor(Color.BLACK);
             for (int rNum = 0; rNum < interRoads.size(); rNum++) { // iterate over incoming roads
+                Road r = interRoads.get(rNum);
+                if (r.getEndRoad()) {
+                    if (r.getNS()) {
+                        if (r.getPF()) {
+                            g2.fillOval(panelX(r.roadEndX + vizRoadWidth/4), panelY(r.roadEndY - vizRoadWidth/2),
+                                    (int)(vizRoadWidth*scalingFactorX), (int)(vizRoadWidth*scalingFactorY));
+                        }
+                        else {
+                            g2.fillOval(panelX(r.roadEndX - 5*vizRoadWidth/4), panelY(r.roadEndY - vizRoadWidth/2),
+                                    (int)(vizRoadWidth*scalingFactorX), (int)(vizRoadWidth*scalingFactorY));
+                        }
+                    }
+                    else {
+                        if (r.getPF()) {
+                            g2.fillOval(panelX(r.roadEndX - vizRoadWidth/2), panelY(r.roadEndY - 5*vizRoadWidth/4),
+                                    (int)(vizRoadWidth*scalingFactorX), (int)(vizRoadWidth*scalingFactorY));
+                        }
+                        else {
+                            g2.fillOval(panelX(r.roadEndX - vizRoadWidth/2), panelY(r.roadEndY + vizRoadWidth/4),
+                                    (int)(vizRoadWidth*scalingFactorX), (int)(vizRoadWidth*scalingFactorY));
+                        }
+                    }
+                }
                 for (Moveable c : interRoads.get(rNum).getCars()) {
                     //stuff;
                 }
             }
+
+
             g2.setColor(Color.BLACK);
             if(i.getSinkScenario() != 0 && i.getSinkScenario() != 6 && i.getSinkScenario() != 7) {
                 // paint roads to the west
