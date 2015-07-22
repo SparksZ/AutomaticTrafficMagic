775ba814c834b93c5cdb1e7a3f161835c0c65282
tdurk93 <tdurk93@gmail.com>
2015-07-21 21:41:40 -0400

    Genetic Algorithm code should be finished
    I tested it with the parameters in Driver.java, and it did worse with
    every generation...

--- src/Driver.java
+++ src/Driver.java
@@ -32,13 +32,13 @@ public class Driver {
     public static final double frameRate = .5; // seconds
     public static final int paintRate = 10; // milliseconds
     public static final int PERIOD = 64;
-    public static final int POPULATION_SIZE = 5; // 20;
-    public static final int NUM_GENERATIONS = 1;
-    public static final int SURVIVORS_PER_GENERATION = 2;
+    public static final int POPULATION_SIZE = 8; // 20;
+    public static final int NUM_GENERATIONS = 5;
+    public static final int SURVIVORS_PER_GENERATION = 4;
     public static final int INDIVIDUALS_TO_CLONE = 2;
     public static final int SIMULATION_TIME = 7200;
     // public static final double INITIAL_MUTATION_P = 0.75;
-    public static final double INITIAL_MUTATION_P = 0.2;
+    public static final double INITIAL_MUTATION_P = 0.1;
     public static final double MUTATION_DECAY_RATE = 0.982;
 
     public static void main(String[] args) {
@@ -52,7 +52,7 @@ public class Driver {
             for (int j = 0; j < numIntersections; j++) {
                 for (int k = 0; k < PERIOD; k++) {
                     lightData[i][j][k] = (byte)prng.nextInt(4);
-                    chromosomes[i][j*k] = lightData[i][j][k];
+                    chromosomes[i][j*PERIOD + k] = lightData[i][j][k];
                 }
             }
         }
@@ -61,50 +61,39 @@ public class Driver {
         futureGenResults = new ArrayList<Future<Results>>(POPULATION_SIZE);
         // run simulation with each chromosome
         for (int i = 0; i < sims.length; i++) {
-            System.out.println("Queuing simulation for individual " + (i + 1) + "...");
             sims[i] = new Simulation(numIntersectionsPerSide, SIMULATION_TIME, lightData[i], i);
             futureGenResults.add(executor.submit(sims[i]));
         }
-        System.out.println("Waiting to finish tasks...");
-        for (Future<Results> fr : futureGenResults) {
+        System.out.println("Executing initial simulations...");
+        for (int i = 0; i < futureGenResults.size(); i++) {
+            Future<Results> fr = futureGenResults.get(i);
             Results r = null;
             try {
                 r = fr.get();
                 generationResults.add(r);
-                System.out.println(r.AVERAGE_SPEED);
+                // System.out.println(lightDataToString(lightData[i]));
+                System.out.println("Individual " + i + " average speed: " + r.AVERAGE_SPEED);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        boolean allFinished = false;
-        /*try {
-            allFinished = executor.awaitTermination(2, java.util.concurrent.TimeUnit.MINUTES);
-        } catch (java.lang.InterruptedException e) { e.printStackTrace(); }
-        if (!allFinished) {
-            System.out.println("Unable to finish queued tasks before timeout!");
-        }
-        else {
-            System.out.println("Whew. All done!");
-        }
-        */
         // iterate over generations:
-        for (int gen = 0; gen < NUM_GENERATIONS; gen++) {
+        for (int gen = 1; gen <= NUM_GENERATIONS; gen++) {
             System.out.println("Starting generation " + gen + ", mutation rate = " + mutationProb);
             // pick the best few
             int[] indicesOfSurvivors = findBestNResults(generationResults, SURVIVORS_PER_GENERATION);
+            // System.out.println("Survivor indices: ["  + indicesOfSurvivors[0] + ", " + indicesOfSurvivors[1] + "]");
             byte[][] survivorChromosomes = new byte[SURVIVORS_PER_GENERATION][lightData[indicesOfSurvivors[0]].length*PERIOD];
             for (int i = 0; i < survivorChromosomes.length; i++) {
                 survivorChromosomes[i] = chromosomes[indicesOfSurvivors[i]];
-                System.out.println("Survivor " + (i + 1) + ": " + lightDataToString(lightData[i]));
+                // System.out.println("Survivor " + (i + 1) + ": " + lightDataToString(lightData[indicesOfSurvivors[i]]));
             }
-            // System.out.println("best: " + indicesOfBestIndividuals[0]);
-            // System.out.println("second best: " + indicesOfBestIndividuals[1]);
             // perform crossover
             // perform mutation
             byte[][][] nextGeneration = new byte[POPULATION_SIZE][numIntersections][PERIOD];
             // randomly sample and do crossovers over random ranges
             for (int childNum = 0; childNum < nextGeneration.length - INDIVIDUALS_TO_CLONE; childNum++) {
-                System.out.println("generating child " + childNum);
+                // System.out.println("generating child " + childNum);
                 int parent1Index, parent2Index;
                 parent1Index = prng.nextInt(survivorChromosomes.length);
                 do {
@@ -114,8 +103,8 @@ public class Driver {
                 byte[] parent2Chromosome = survivorChromosomes[parent2Index];
                 int crossoverStart = prng.nextInt(parent1Chromosome.length - 1); // -1 to make sure the last index isn't picked
                 int crossoverEnd = crossoverStart + 1 + prng.nextInt(parent1Chromosome.length - crossoverStart - 1);
-                System.out.println("child crossoverStart = " + crossoverStart);
-                System.out.println("child crossoverEnd = " + crossoverEnd);
+                // System.out.println("child crossoverStart = " + crossoverStart);
+                // System.out.println("child crossoverEnd = " + crossoverEnd);
                 byte[] child = new byte[parent1Chromosome.length];
                 for (int i = 0; i < crossoverStart; i++) {
                     if (prng.nextDouble() < mutationProb) {
@@ -144,21 +133,42 @@ public class Driver {
                     }
                     nextGeneration[childNum][i/PERIOD][i % PERIOD] = child[i];
                 }
-                System.out.println("Child " + (childNum + 1) + ": " + lightDataToString(nextGeneration[childNum]));
+                // System.out.println("Child " + (childNum + 1) + ": " + lightDataToString(nextGeneration[childNum]));
             }
 
             // also, clone 2 of the best
             int[] indicesOfBestIndividuals = findBestNResults(generationResults, INDIVIDUALS_TO_CLONE);
+            // System.out.println("Best individuals indices: ["  + indicesOfBestIndividuals[0] + ", " + indicesOfBestIndividuals[1] + "]");
             byte[][][] bestIndividuals = new byte[INDIVIDUALS_TO_CLONE][lightData[indicesOfBestIndividuals[0]].length][PERIOD];
             for (int i = 0; i < bestIndividuals.length; i++) {
                 bestIndividuals[i] = lightData[indicesOfBestIndividuals[i]];
                 int newIndex = nextGeneration.length - INDIVIDUALS_TO_CLONE + i;
                 nextGeneration[newIndex] = bestIndividuals[i];
-                System.out.println("Clone " + (i + 1) + "(index " + newIndex +  "): " + lightDataToString(bestIndividuals[i]));
+                // System.out.println("Clone " + (i + 1) + "(index " + newIndex +  "): " + lightDataToString(bestIndividuals[i]));
             }
-            System.exit(0);
             // run simulation
-            // executor.submit(sims[i]);
+            for (int i = 0; i < sims.length; i++) {
+                sims[i] = new Simulation(numIntersectionsPerSide, SIMULATION_TIME, nextGeneration[i], gen*i);
+                futureGenResults.set(i, executor.submit(sims[i]));
+            }
+            System.out.println("Executing simulation in generation " + gen + "...");
+            for (int i = 0; i < futureGenResults.size(); i++) {
+                Future<Results> fr = futureGenResults.get(i);
+                Results r = null;
+                try {
+                    r = fr.get();
+                    generationResults.set(i, r);
+                    System.out.println("Individual " + i + " average speed: " + r.AVERAGE_SPEED);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            double generationAverageSpeed = 0;
+            for (Results r : generationResults) {
+                generationAverageSpeed += r.AVERAGE_SPEED;
+            }
+            generationAverageSpeed /= generationResults.size();
+            System.out.println("Generation-wide average speed: " + generationAverageSpeed);
             mutationProb *= MUTATION_DECAY_RATE;
         }
         executor.shutdown();
@@ -183,19 +193,19 @@ public class Driver {
 
     private static int[] findBestNResults(ArrayList<Results> allResults, int n) {
         int[] bestResults = new int[n];
-        ArrayList<Results> resultsCopy = (ArrayList<Results>)allResults.clone();
+        // ArrayList<Results> resultsCopy = (ArrayList<Results>)allResults.clone();
+        ArrayList<Integer> indices = new ArrayList<Integer>(n);
         for (int m = 0; m < n; m++) {
             double bestAvgSpeed = 0;
             int bestIndex = -1;
-            for (int i = 0; i < resultsCopy.size(); i++) {
-                Results currResult = resultsCopy.get(i);
-                if (currResult.AVERAGE_SPEED > bestAvgSpeed) {
+            for (int i = 0; i < allResults.size(); i++) {
+                Results currResult = allResults.get(i);
+                if (currResult.AVERAGE_SPEED > bestAvgSpeed && !indices.contains(new Integer(i))) {
                     bestAvgSpeed = currResult.AVERAGE_SPEED;
                     bestIndex = i;
                 }
             }
-            // can't remove because that would change the indices on future iterations over m
-            resultsCopy.set(bestIndex, new Results(0, 1, 0, -1)); // set to a new value that will never be "best"
+            indices.add(bestIndex);
             bestResults[m] = bestIndex;
         }
         return bestResults;
